9296d5e65abfa002280aa050b8d827798469aa6d
kevinqlfang <102271270+kevinqlfang@users.noreply.github.com>
2023-03-02 00:06:17 -0500

    define a view
    list all projects and allows creating new projects using the 'ProjectSerializer'

--- my_portfolio/projects/views.py
+++ my_portfolio/projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
+from .serializers import ProjectSerializer
+from rest_framework import generics
 
-# Create your views here.
+class ProjectListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
