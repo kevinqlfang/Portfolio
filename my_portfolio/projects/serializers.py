add870b4cc6613e98c629de2fda55f45ff5411d3
kevinqlfang <102271270+kevinqlfang@users.noreply.github.com>
2023-03-02 00:06:27 -0500

    Revert "define serializer"
    This reverts commit 10e385501785bdd4f95adfdb4f767b88a78a3c08.

--- my_portfolio/projects/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Project
-
-# define a serializer for the Project model that includes all of the fields
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
